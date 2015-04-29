363bd8497cb47165b7586f6689594879efef63d5
Makers Laptop 66 <makerslaptop66@Makerss-MacBook-Pro-2.local>
2015-04-29 12:43:45 +0100

    added test

---
+++ spec/features/public_accesses_bike_spec.rb
@@ -0,0 +1,9 @@
+require 'docking_station'
+
+feature 'member of public accesses bike'  do 
+	scenario 'docking station releases working bike' do
+		docking_station = DockingStation.new
+		bike = docking_station.release_bike
+		expect(bike).to be_working
+	end
+end
