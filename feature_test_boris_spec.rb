0d028ea3c70784113ba2734f866c6dd2e6212362
Makers Laptop 66 <makerslaptop66@Makerss-MacBook-Pro-2.local>
2015-04-29 19:37:29 +0100

    created a bike spec file and got rid of responds to working in docking station spec file

--- feature_test_boris_spec.rb
+++
@@ -1,7 +0,0 @@
-feature "Docking station releases bikes" do 
-	scenario "bike is released to user from docking station" do
-		station = DockingStation.new
-		bike = station.release_bike
-		expect(bike).to be_working?
-	end
-end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,5 +2,4 @@ require 'dockingstation'
 
 describe DockingStation  do
   it { is_expected.to respond_to :release_bike }
-  it { is_expected.to respond_to :working? }
 end
