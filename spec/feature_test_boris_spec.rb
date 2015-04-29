6b2b5dcba59f3a17ab529a983b73d7b522845aa9
Makers Laptop 93 <makerslaptop93@Makerss-MacBook-Pro-4.local>
2015-04-29 19:18:34 +0100

    added to dockingstation unit test

--- spec/feature_test_boris_spec.rb
+++ spec/feature_test_boris_spec.rb
@@ -1,5 +1,5 @@
-feature "Docking station releases bikes" do 
-	scenario "bike is released to user from docking station" do
+feature "Docking station releases bikes" do
+	scenario "Bike is released to user from docking station" do
 		station = DockingStation.new
 		bike = station.release_bike
 		expect(bike).to be_working?
